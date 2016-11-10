73ce58713f07863f4f918fc22f2874d9c893f4e5
Kacy Hulme (kacyh) <kacyhulme@gmail.com>
2016-11-10 12:44:16 -0600

    progress on get day and time functions

--- public/js/heatwave.js
+++ public/js/heatwave.js
@@ -1,17 +1,70 @@
-var getName = function() {
-	var x = document.getElementById("daysofweek").value;
-	if (x == "monday") {
-		console.log("yo sup");
-	}
-	else {
-		console.log("yo nooo");
-	}
 
-	var y = document.getElementById("hourofday").value;
-	console.log(y);	
+var getDay = function() {
+	var getx = function() {
+		var x = document.getElementById("daysofweek").value;
+		return x;
+	}
+	switch ("monday") {
+		case "monday":
+			daygradient = "5";
+			break;
+		case "tuesday":
+			daygradient = "10";
+			break;
+		case "wednesday":
+			daygradient = "15";
+			break;
+		case "thursday":
+			daygradient = "20";
+			break;
+		case "friday":
+			daygradient = "25";
+			break;
+		case "saturday":
+			daygradient = "30";
+			break;
+		case "sunday":
+			daygradient = "35";
+	}	
+	return daygradient
 }
 
-document.getElementById("submit").addEventListener("click", getName);
+var getHour = function() {
+	var gety = function() {
+		var y = document.getElementById("hourofday").value;
+		return y;
+	}
+	switch (gety()) {
+		case "8am-12pm":
+		hourgradient = "5";
+		break;
+		case "12pm-4pm":
+		hourgradient = "10";
+		break;
+		case "4pm-8pm":
+		hourgradient = "15";
+		break;
+		case "8pm-12am":
+		hourgradient = "20";
+		break;
+		case "12am-4am":
+		hourgradient = "25";
+		break;
+		case "4am-8am":
+		hourgradient = "30";
+	}
+	return hourgradient
+}
+
+
+var getName = function() {
+	g = getHour();
+	d = getDay();
+	console.log([g,d])
+	return [g,d];
+}
+
+document.getElementById("submit").addEventListener("click", getName());
 
 var drawHeat = function() {
 	var c = document.getElementById("canvas");
@@ -29,25 +82,24 @@ var drawHeat = function() {
 
 
 ////////
- function loadJSON(callback) {   
+function loadJSON(callback) {   
 
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
     xobj.open('GET', 'data.json', true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
+    	if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
             callback(xobj.responseText);
-          }
+        }
     };
     xobj.send(null);  
- }
-
- function init() {
- loadJSON(function(response) {
-  // Parse JSON string into object
-    var actual_JSON = JSON.parse(response);
-    console.log(actual_JSON);
- });
 }
- 
+
+function init() {
+	loadJSON(function(response) {
+  // Parse JSON string into object
+  var actual_JSON = JSON.parse(response);
+  console.log(actual_JSON);
+});
+}
