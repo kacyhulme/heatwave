2478402a5c292edf93a02ba5d413300a4c0badbf
Kacy Hulme (kacyh) <kacyhulme@gmail.com>
2016-11-10 12:47:32 -0600

    make functions wait

--- public/js/heatwave.js
+++ public/js/heatwave.js
@@ -58,10 +58,16 @@ var getHour = function() {
 
 
 var getName = function() {
-	g = getHour();
-	d = getDay();
-	console.log([g,d])
-	return [g,d];
+	setTimeout(function(){ 
+		g = getHour(); 
+	}, 1000);
+	setTimeout(function(){ 
+		d = getDay(); 
+	}, 2000);
+	setTimeout(function(){ 
+		console.log([g,d])
+		return [g,d];
+	}, 3000);
 }
 
 document.getElementById("submit").addEventListener("click", getName());
@@ -79,27 +85,3 @@ var drawHeat = function() {
 	ctx.fillRect(0,0,600,200);
 }();
 
-
-
-////////
-function loadJSON(callback) {   
-
-	var xobj = new XMLHttpRequest();
-	xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'data.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-    	if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);  
-}
-
-function init() {
-	loadJSON(function(response) {
-  // Parse JSON string into object
-  var actual_JSON = JSON.parse(response);
-  console.log(actual_JSON);
-});
-}
